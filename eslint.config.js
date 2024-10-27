dce99e19fe2f74978710ee6e99425776604e16ee
Nikita Lisovyi <nykyta.lisovyi@nure.ua>
2024-10-27 16:29:06 +0200

    Javascript liter test 8

--- eslint.config.js
+++ eslint.config.js
@@ -2,9 +2,13 @@ const { defineConfig } = require("eslint-define-config");
 
 module.exports = defineConfig({
   languageOptions: {
+    parser: "@babel/eslint-parser",
     parserOptions: {
       ecmaVersion: 2020,
       sourceType: "module",
+      ecmaFeatures: {
+        jsx: true,
+      },
     },
   },
 });
