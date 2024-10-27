2ed3f28561c2a8ce2d6469cdb86f6acd2c81afb8
Nikita Lisovyi <nykyta.lisovyi@nure.ua>
2024-10-27 16:35:35 +0200

    Javascript liter test 11

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,15 @@ const { defineConfig } = require("eslint-define-config");
 const babelEslint = require("@babel/eslint-parser");
 
 module.exports = defineConfig({
-  parser: babelEslint,
-  parserOptions: {
-    requireConfigFile: false,
-    ecmaVersion: 2020,
-    sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
+  languageOptions: {
+    parser: "@babel/eslint-parser", // Use the parser you need
+    parserOptions: {
+      requireConfigFile: false,
+      ecmaVersion: 2020,
+      sourceType: "module",
+      ecmaFeatures: {
+        jsx: true, // Enable JSX if you're using React
+      },
     },
   },
 });
