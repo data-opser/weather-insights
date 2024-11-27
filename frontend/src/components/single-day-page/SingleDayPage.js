cf16bcf313ad04665e40c95799a2dec180f68152
OleksandrKozhanov <sanchokozhanov@gmail.com>
2024-11-27 17:56:10 +0200

    hourly block moved to homepage, pagelink deleted

--- frontend/src/components/single-day-page/SingleDayPage.js
+++ frontend/src/components/single-day-page/SingleDayPage.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
 import "./SingleDayPage.css";
-import DayList from "./DayList";
 import Hour from "./Hour";
 import api from "../axiosConfig";
 import { HiOutlineCog6Tooth } from "react-icons/hi2";
 
-const SingleDayPage = () => {
-  const { date, cityId } = useParams();
+const SingleDayPage = ({ date, cityId }) => {
   const [hourlyData, setHourlyData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -43,32 +40,29 @@ const SingleDayPage = () => {
       )}
       {error && <h1 style={{ color: "red" }}>{error}</h1>}
       {!loading && !error && (
-        <>
-          <DayList />
-          <div className="hour-table">
-            <div className="hour-table-main-text">
-              <p className="hour-table-main-text-feels">It feels</p>
-              <p className="hour-table-main-text-pressure">Pressure, mm</p>
-              <p className="hour-table-main-text-humidity">Humidity, %</p>
-              <p className="hour-table-main-text-wind">Wind, m/s</p>
-              <p className="hour-table-main-text-precipitation">Precipitation probability, %</p>
-            </div>
-            <div className="hour-columns">
-              {hourlyData.map((hour, index) => (
-                <Hour
-                  key={index}
-                  time={hour.time}
-                  temperature={hour.temperature}
-                  temperature_feels_like={hour.temperature_feels_like}
-                  pressure={hour.pressure}
-                  humidity={hour.humidity}
-                  wind_speed={hour.wind_speed}
-                  precipitation={hour.precipitation}
-                />
-              ))}
-            </div>
+        <div className="hour-table">
+          <div className="hour-table-main-text">
+            <p className="hour-table-main-text-feels">It feels</p>
+            <p className="hour-table-main-text-pressure">Pressure, mm</p>
+            <p className="hour-table-main-text-humidity">Humidity, %</p>
+            <p className="hour-table-main-text-wind">Wind, m/s</p>
+            <p className="hour-table-main-text-precipitation">Precipitation probability, %</p>
           </div>
-        </>
+          <div className="hour-columns">
+            {hourlyData.map((hour, index) => (
+              <Hour
+                key={index}
+                time={hour.time}
+                temperature={hour.temperature}
+                temperature_feels_like={hour.temperature_feels_like}
+                pressure={hour.pressure}
+                humidity={hour.humidity}
+                wind_speed={hour.wind_speed}
+                precipitation={hour.precipitation}
+              />
+            ))}
+          </div>
+        </div>
       )}
     </div>
   );
