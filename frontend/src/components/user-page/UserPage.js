d7ca1e7c96d6126e105f32bacb84e0be8259a358
Jekaega <jekaaega228@gmail.com>
2024-11-27 21:28:00 +0200

    added more styles for accordion and userpage at all

--- frontend/src/components/user-page/UserPage.js
+++ frontend/src/components/user-page/UserPage.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './UserPage.css';
-import user from './user.jpg';
 import { GoPlus } from "react-icons/go";
+import { BsPerson } from "react-icons/bs";
+import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
 import raincloud from './raincloud.jfif'
 
 const UserPage = () => {
@@ -26,41 +27,97 @@ const UserPage = () => {
       <div className="user">
 
         <div className='user-block1'>
-          <img className='user-photo' src={user}></img>
+          <BsPerson className='user-photo' />
           <GoPlus
             className={`plus-photo ${activeAddPhotoButton ? 'gray' : ''}`}
             onClick={handleAddPhotoButtonClick}
           />
         </div>
-
-        <p className='user-block2-text-gray'>Last login: Nov. 28, 2024, 15:30</p>
-        <p className='user-text-bold'>My profile</p>
-          
-        <div className='faq'>
-          <div className='faq-item'>
-            <label className='faq-title' for='faq_1'>Name</label>
-            <input className='faq-input' type='checkbox' id='faq_1'></input>
-            <div className='faq-text'>
-              <p>lipsumfgggfg fgfgdggf fghfghfghgfhfg</p>
-            </div>
-          </div>          
-          <div className='faq-item'>
-            <label className='faq-title' for='faq_2'>Date of birth</label>
-            <input className='faq-input' type='checkbox' id='faq_2'></input>
-            <div className='faq-text'>
-              <p>lipsumfgggfg fgfgdggf fghfghfghgfhfg</p>
-            </div>
-          </div>
-
+        <div className='profile-info'>
+          <p className='user-text-bold'>My profile</p>
+          <p className='user-block2-text-gray'>Created at: Nov. 28, 2024, 15:30</p>
         </div>
 
-        <button
-          className={`user-button ${activeSaveButton ? 'blue' : ''}`}
-          onClick={handleSaveButtonClick}>
-          Save
-        </button>
 
+
+        <div className='faq'>
+          <div className='faq-item'>
+            <label className='faq-title-email'><p className='faq-title-type'>Email</p><p className='faq-title-text'>anton.reshetniak@nure.ua</p></label>
+          </div>
+
+          <div className='faq-item'>
+            <input className='faq-input' type='checkbox' id='faq_1'></input>
+            <label className='faq-title' for='faq_1'><p className='faq-title-type'>Name</p><p className='faq-title-text'>Anton Reshetniak</p></label>
+            <label className='faq-label-arrow-top' for="faq_1">
+              <AiOutlineDown className="faq-arrow-top" />
+            </label>
+
+            <form className='faq-text'>
+              <input placeholder='Enter your new name'></input>
+              <button
+                type='submit'
+                className={`user-button ${activeSaveButton ? 'blue' : ''}`}
+                onClick={handleSaveButtonClick}>
+                Save
+              </button>
+              <div className='faq-status'>
+                <p>Successfully</p>
+              </div>
+            </form>
+
+          </div>
+
+          <div className='faq-item'>
+            <input className='faq-input' type='checkbox' id='faq_2'></input>
+            <label className='faq-title' for='faq_2'><p className='faq-title-type'>Date of birth</p><p className='faq-title-text padding-left'>03.04.2005</p></label>
+            <label className='faq-label-arrow-top' for="faq_2">
+              <AiOutlineDown className="faq-arrow-top" />
+            </label>
+
+            <form className='faq-text'>
+              <input placeholder='Enter your new date of birth'></input>
+              <button
+                type='submit'
+                className={`user-button ${activeSaveButton ? 'blue' : ''}`}
+                onClick={handleSaveButtonClick}>
+                Save
+              </button>
+              <div className='faq-status'>
+                <p>Successfully</p>
+              </div>
+            </form>
+          </div>
+
+
+
+          <div className='faq-item'>
+            <input className='faq-input' type='checkbox' id='faq_3'></input>
+            <label className='faq-title' for='faq_3'><p className='faq-title-type'>Password</p></label>
+            <label className='faq-label-arrow-top' for="faq_3">
+              <AiOutlineDown className="faq-arrow-top" />
+            </label>
+
+            <form className='faq-text'>
+              <input placeholder='Enter your old password'></input>
+              <div>
+                <input placeholder='Enter your new password'></input>
+                <button
+                  type='submit'
+                  className={`user-button ${activeSaveButton ? 'blue' : ''}`}
+                  onClick={handleSaveButtonClick}>
+                  Save
+                </button>
+                <div className='faq-status'>
+                  <p>Successfully</p>
+                </div>
+              </div>
+
+            </form>
+          </div>
+        </div>
       </div>
+
+
 
       <div className="notifications">
         <p className='user-text-bold'>Last notifications</p>
