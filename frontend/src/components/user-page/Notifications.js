b46d40af7fecf3bc8b5c1109ff728ad393157979
Oleksandr Kozhanov <oleksandr.kozhanov@nure.ua>
2024-12-21 19:07:07 +0200

    changed dates when to remind

--- frontend/src/components/user-page/Notifications.js
+++ frontend/src/components/user-page/Notifications.js
@@ -197,7 +197,7 @@ const Notifications = () => {
         </div>
         <p>Sending date</p>
         <div className="radio-buttons-block">
-          {[15, 10, 5, 3, 1, 0].map((day) => (
+          {[15, 10, 7, 3, 1, 0].map((day) => (
             isDateValid(day) && (
               <label key={day}>
                 <input
