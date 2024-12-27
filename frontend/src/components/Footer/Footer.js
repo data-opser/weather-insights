066017a00c6e6bbcff522029da3c8f58b1ce563b
Jekaega <jekaaega228@gmail.com>
2024-12-27 17:53:13 +0200

    added new elements to the footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,9 +1,30 @@
 import './Footer.css';
+import logo from '../Header/AuthForm/form-weather-icon.png';
+import { BsQrCode } from "react-icons/bs";
+import { LuPhoneCall } from "react-icons/lu";
+import { BsEnvelope } from "react-icons/bs";
 
 function Footer() {
   return (
     <div className="footer">
-      <p>&copy; 2024 Weather Insights</p>
+      <div className='footer-info'>
+        <div className='footer-logo'>
+          <img className="logo" src={logo} alt="company-logo"></img>
+          <p className='lastblock'>&copy; 2024 Weather Insights</p>
+        </div>
+        <div className='mobile-app'>
+          <p>Mobile application</p>
+          <BsQrCode className='app-qrcode' />
+        </div>
+        <div className='phone-number'>
+          <LuPhoneCall />
+          <p>+380667751067</p>
+        </div>
+        <div className='our-email'>
+          <BsEnvelope />
+          <p>jekaaega228@gmail.com</p>
+        </div>
+      </div>
     </div>
   );
 }
